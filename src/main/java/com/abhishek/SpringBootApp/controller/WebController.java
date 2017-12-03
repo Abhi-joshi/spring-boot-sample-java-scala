20527c8c27cad067091088c5e9b1df7bb5a5ebee
Abhishek Sharma <mail.abhishekjoshi@gmail.com>
2017-12-03 19:46:11 +0530

    Rest Service Documentation using Swagger.

--- src/main/java/com/abhishek/SpringBootApp/controller/WebController.java
+++ src/main/java/com/abhishek/SpringBootApp/controller/WebController.java
@@ -1,64 +1,65 @@
 package com.abhishek.SpringBootApp.controller;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.abhishek.SpringBootApp.model.Customer;
 import com.abhishek.SpringBootApp.repo.CustomerRepository;
- 
+import io.swagger.annotations.Api;
+
 @RestController
+@RequestMapping("/customer")
+@Api(value = "customer details", description = "Operations about Customer Details")
 public class WebController {
 	@Autowired
 	CustomerRepository repository;
-	
-	@RequestMapping("/save")
-	public String process(){
-		// save a single Customer
-		repository.save(new Customer("Jack", "Smith"));
-		
-		// save a list of Customers
-		repository.save(Arrays.asList(new Customer("Adam", "Johnson"), new Customer("Kim", "Smith"),
-										new Customer("David", "Williams"), new Customer("Peter", "Davis")));
-		
-		return "Done";
-	}
-	
-	
-	@RequestMapping("/findall")
-	public List<Customer> findAll(){
-		
-		List<Customer> customerList = new ArrayList<Customer>(); 
-		for(Customer cust : repository.findAll()){
+
+	@RequestMapping(value = "/list", method = RequestMethod.GET, produces = "application/json")
+	public List<Customer> list() {
+
+		List<Customer> customerList = new ArrayList<Customer>();
+		for (Customer cust : repository.findAll()) {
 			Customer customer = new Customer(cust.getId(), cust.getFirstName(), cust.getLastName());
 			customerList.add(customer);
 		}
-		
+
 		return customerList;
 	}
-	
-	@RequestMapping("/findbyid")
-	public Customer findById(@RequestParam("id") long id){
-		
+
+	@RequestMapping(value = "/show/{id}", method = RequestMethod.GET, produces = "application/json")
+	public Customer showProduct(@PathVariable Long id) {
 		Customer customer = repository.findOne(id);
 		return customer;
 	}
-	
-	@RequestMapping("/findbylastname")
-	public List<Customer> fetchDataByLastName(@RequestParam("lastname") String lastName){
-		
-		List<Customer> customerList = new ArrayList<Customer>();
-		
-		for(Customer cust: repository.findByLastName(lastName)){
-			Customer customer = new Customer(cust.getId(), cust.getFirstName(), cust.getLastName());
-			customerList.add(customer);
-		}
-		
-		return customerList;
+
+	@RequestMapping(value = "/add", method = RequestMethod.POST, produces = "application/json")
+	public ResponseEntity<String> saveProduct(@RequestBody Customer customer) {
+		repository.save(customer);
+		return new ResponseEntity<String>("Customer details saved successfully", HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/update/{id}", method = RequestMethod.PUT, produces = "application/json")
+	public ResponseEntity<String> updateProduct(@PathVariable Long id, @RequestBody Customer customer) {
+		Customer cust = repository.findOne(id);
+		cust.setFirstName(customer.getFirstName());
+		cust.setLastName(customer.getLastName());
+		repository.save(cust);
+		return new ResponseEntity<String>("Customer deatils updated successfully", HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE, produces = "application/json")
+	public ResponseEntity<String> delete(@PathVariable Long id) {
+		repository.delete(id);
+		return new ResponseEntity<String>("Customer details deleted successfully", HttpStatus.OK);
+
 	}
 }
